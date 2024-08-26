d3e9365016b2b43f87bc3c43e2a650407e3ef4e0
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-08-26 21:26:24 +0000

    conversãp de dados

--- tipos_de_dados.py
+++ tipos_de_dados.py
@@ -4,6 +4,7 @@ print(True)
 print(False)
 print("Python")
 
+'''
 int() #inteiro
 float() #decimais/flutuantes
 str() #string
@@ -18,3 +19,44 @@ dict() #mapeamento
 set() , frozenset() #conjuntos/coleções
 bool() #booleano
 bytes() , bytearray() , memoryview() #binários
+'''
+
+# conversão de tipos
+price = 10
+print("O preço é " + str(price))
+
+#inteiro para float (decimal)
+price = float(price)
+print(price)
+
+price = 10/2
+print(price)
+
+# conversão por divisão
+price = 10
+price = (price)
+
+print(price/2)
+
+print(price//2)
+
+# conversão de numerico para string
+price = 10.50
+age = 20
+
+print("O preço é " + str(price))
+print("A idade é " + str(age))
+texto = f"O preço é {price} e a idade é {age}" #f-string (formatação de string) - concatenação de variáveis
+print(texto) 
+
+# conversão de string para numerico
+price = "10.50"
+age = "20"
+
+print(float(price))
+print(int(age))
+
+# erro de conversão
+
+price = "python"
+print(float(price)) #ValueError
