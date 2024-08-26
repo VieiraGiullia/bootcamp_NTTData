b1f04c21b9d80b18195cdd0bfebdc850330d04f6
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-08-26 23:05:22 +0000

    operadores logicos

--- sintaxe_basica/operadores.py
+++ sintaxe_basica/operadores.py
@@ -71,3 +71,20 @@ print(saldo)
 # atribuição com exponenciação
 saldo **= 3
 print(saldo)
+
+# Operadores lógicos
+# são utilizados para combinar valores booleanos e retornar
+
+saldo = 450
+saque = 400
+limite = 500
+
+# operador E (and)
+print(saldo > saque and saldo < limite)
+
+# operador OU (or)
+print(saldo > saque or saldo < limite)
+
+# operador NÃO (not)
+print(not saldo > saque)
+
