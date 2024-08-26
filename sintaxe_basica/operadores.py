99baeb8d2900bdb7a1a60378b6102bd4120d3343
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-08-26 23:18:01 +0000

    operadores de associação

--- sintaxe_basica/operadores.py
+++ sintaxe_basica/operadores.py
@@ -96,3 +96,16 @@ nome_da_rua = rua
 
 rua is nome_da_rua # ambos apontam para o mesmo objeto
 rua is not nome_da_rua # ambos apontam para objetos diferentes
+
+# Operadores de associação
+# são utilizados para verificar se um objeto está presente em outro objeto
+
+rua = 'Rua 1'
+carros = ['carro1', 'carro2', 'carro3']
+valores = [1, 2, 3, 4, 5]
+
+"Otacilio" in rua # verifica se a string 'Otacilio' está presente na variável rua
+
+"carro4" not in carros # verifica se a string 'carro4' não está presente na lista carros
+
+8 in valores # verifica se o número 8 está presente na lista valores
