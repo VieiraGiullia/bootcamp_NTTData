ef10a772cc14f4639050503416130a299a487429
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-08-26 20:55:05 +0000

    variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,22 @@
+# Variaveis são valores que podem ser alterados, já constantes são valores que não podem ser alterados.
+
+# exemplo de variavel
+
+age = 20
+name = "João"
+print(name, "tem", age, "anos")
+
+age = 30
+name = "Maria"
+print(name, "tem", age, "anos")
+
+# Python não tem constante!!! Mas podemos usar variaveis em caixa alta para simular uma constante.
+
+# exemplo de constante
+
+ABS_PATH = "/home/user"
+DEBUG = True
+STATES = ["SP", "RJ", "MG", "ES"]
+AMOUNT = 30.2
+
+print(ABS_PATH)
