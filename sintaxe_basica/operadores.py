59a8c29c807b9930b94bfad8b4bdbc61b4808a56
Giullia Vieria da Cunha <130769319+VieiraGiullia@users.noreply.github.com>
2024-08-26 22:44:53 +0000

    operadores aritmeticos

---
+++ sintaxe_basica/operadores.py
@@ -0,0 +1,23 @@
+# operadores aritmeticos 
+# sõa utilizados para realizar operações matemáticas
+
+# adição
+print(2 + 2)
+
+#subtração
+print(2 - 2)
+
+#multiplicação
+print(2 * 2)
+
+#divisão e divisão inteira
+print(12 / 3)
+print(12 // 2)
+
+#modulo e exponenciação
+print(10 % 3)
+print(2 ** 3)
+
+# precedência de operadores (é igula a matemática)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
